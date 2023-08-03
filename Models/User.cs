a7655c0fcb34a539075ba00e767bdb4dcd5b8e76
KAROUI-FARES <126524113+KAROUIFARES@users.noreply.github.com>
2023-08-03 17:49:15 +0200

    v2.3

--- Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace DotnetToken.Models
-{
-    public class User
-    {
-        public string UserName {get;init;}=string.Empty;
-        public string PasswordHash{get;init;}=string.Empty;
-    }
-}
--- Models/UserDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DotnetToken.Models
-{
-    public class UserDto
-    {
-        public required string UserName{get;init;}
-        public required string Password{get;init;}
-    }
-}
